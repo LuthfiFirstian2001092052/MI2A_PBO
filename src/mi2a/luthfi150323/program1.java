53c2dd9e63e55cb08a2744f7d76dc7cf017c4e6e
LuthfiFirstian2001092052 <114200621+LuthfiFirstian2001092052@users.noreply.github.com>
2023-03-15 11:00:01 +0700

    First Commit

---
+++ src/mi2a/luthfi150323/program1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mi2a.luthfi150323;
+
+/**
+ *
+ * @author Akula
+ */
+public class program1 {
+    
+}
