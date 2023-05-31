959bd856e8d93812ef21a9c05137f73ec6729f38
LuthfiFirstian2001092052 <114200621+LuthfiFirstian2001092052@users.noreply.github.com>
2023-05-31 12:33:33 +0700

    First Commit

---
+++ src/UTS_PBO2_LuthfiFirstian2001092052/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS_PBO2_LuthfiFirstian2001092052;
+import UTS_PBO2_LuthfiFirstian2001092052.TransferModel;
+
+/**
+ *
+ * @author Akula
+ */
+public class InformasiSaldoModel {
+    String saldo="6900000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/UTS_PBO2_LuthfiFirstian2001092052/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS_PBO2_LuthfiFirstian2001092052;
+
+/**
+ *
+ * @author Akula
+ */
+public class PinModel {
+   
+    String pin = "069420";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
